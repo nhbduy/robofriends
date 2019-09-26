8e0632a2c9201176194d6bd3cddaf7e99e62101f
Duy Nguyen <duy.nguyen@christinas.vn>
2019-09-26 10:04:24 +0700

    Updates

---
+++ precache-manifest.a35308f8070776ff19adf65b75be12a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b9101c4202fd08b51338c3220c8f0f9",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "96b76fadc365132d1a92",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "11529094ca4be74c77ff",
+    "url": "/robofriends/static/css/main.4cfaddbd.chunk.css"
+  },
+  {
+    "revision": "96b76fadc365132d1a92",
+    "url": "/robofriends/static/js/2.499128b4.chunk.js"
+  },
+  {
+    "revision": "11529094ca4be74c77ff",
+    "url": "/robofriends/static/js/main.514e7878.chunk.js"
+  },
+  {
+    "revision": "d7d3c3ad781ab5e09c65",
+    "url": "/robofriends/static/js/runtime-main.a9498424.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
