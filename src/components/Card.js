b194956a621f2c9d869385e2cd341986267fa22e
Hoang-Bao-Duy NGUYEN <duy3105@gmail.com>
2019-10-19 20:25:41 +0700

    Get 150x150 image size

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ id, name, username, email }) => {
   return (
     <div className='tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img src={`https://robohash.org/${id}?size=50x50`} alt={username} />
+      <img src={`https://robohash.org/${id}?size=150x150`} alt={username} />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
