3c75f310f2630a374035367f92e0fb874e1db55a
Hoang-Bao-Duy NGUYEN <duy3105@gmail.com>
2019-10-19 20:44:15 +0700

    Register service worker on production

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
 
 if (process.env.NODE_ENV !== 'production') {
   const whyDidYouRender = require('@welldone-software/why-did-you-render/dist/no-classes-transpile/umd/whyDidYouRender.min.js');
