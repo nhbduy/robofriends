fa098fe8377e4c9663588d535c6ec319241cf892
Hoang-Bao-Duy NGUYEN <duy3105@gmail.com>
2019-10-19 20:20:49 +0700

    Add aria-label that screen readers can use

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,6 +4,7 @@ const Search = ({searchChange}) => {
   return (
     <div className='pa2'>
       <input 
+        aria-label='Search Robots'
         className='pa3 b--blue bg-lightest-blue' 
         type='search' 
         placeholder='search robots...' 
