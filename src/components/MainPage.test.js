ffa440f3e3389b2c1b55a8593a5f0b1b11d39542
Duy Nguyen <duy.nguyen@christinas.vn>
2019-10-23 19:03:15 +0700

    Enzyme testing with connected component: MainPage

--- src/components/MainPage.test.js
+++ src/components/MainPage.test.js
@@ -1,12 +1,67 @@
 import React from 'react';
 
 import { shallow } from 'enzyme';
-import App from '../containers/App';
+import MainPage from './MainPage';
 
-it('expect to render App component', () => {
-  const mockStore = {
+let wrapper;
+beforeEach(() => {
+  const mockProps = {
+    onRequestRobots: jest.fn(),
     robots: [],
-    searchField: ''
+    searchField: '',
+    isPending: false
   };
-  expect(shallow(<App store={mockStore} />).debug()).toMatchSnapshot();
+
+  wrapper = shallow(<MainPage {...mockProps} />);
+});
+
+it('expect to render MainPage component', () => {
+  expect(wrapper.debug()).toMatchSnapshot();
+});
+
+it('filters robots correctly', () => {
+  const mockProps = {
+    onRequestRobots: jest.fn(),
+    robots: [
+      {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz'
+      }
+    ],
+    searchField: 'John',
+    isPending: false
+  };
+
+  wrapper = shallow(<MainPage {...mockProps} />);
+  expect(wrapper.instance().filteredRobots()).toEqual([]);
+});
+
+it('filters robots correctly 2', () => {
+  const mockProps = {
+    onRequestRobots: jest.fn(),
+    robots: [
+      {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz'
+      }
+    ],
+    searchField: 'Leanne',
+    isPending: false
+  };
+
+  const filteredResult = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      username: 'Bret',
+      email: 'Sincere@april.biz'
+    }
+  ];
+
+  wrapper = shallow(<MainPage {...mockProps} />);
+  expect(wrapper.instance().filteredRobots()).toEqual(filteredResult);
 });
