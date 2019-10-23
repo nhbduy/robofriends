b1aa61827058f31f94b5065f80b7ef03277188a8
Duy Nguyen <duy.nguyen@christinas.vn>
2019-10-23 20:43:04 +0700

    Export apiCall function

--- src/actions.js
+++ src/actions.js
@@ -3,19 +3,21 @@ import {
   REQUEST_ROBOTS_PENDING,
   REQUEST_ROBOTS_SUCCESS,
   REQUEST_ROBOTS_FAILED
-} from './constants'
+} from './constants';
+
+import { apiCall } from './api/api';
 
 // return object
-export const setSearchField = (text) => ({
+export const setSearchField = text => ({
   type: CHANGE_SEARCH_FIELD,
   payload: text
 });
 
 // return function
-export const setRequestRobots = () => (dispatch) => {
+export const setRequestRobots = () => dispatch => {
   dispatch({ type: REQUEST_ROBOTS_PENDING });
-  fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
+
+  apiCall('https://jsonplaceholder.typicode.com/users')
     .then(data => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data }))
-    .catch(error => dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error }))
-}
+    .catch(error => dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error }));
+};
---
+++ src/api/api.js
@@ -0,0 +1,1 @@
+export const apiCall = link => fetch(link).then(response => response.json());
