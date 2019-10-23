35f3f480e0bad3557d8a8d208a11aa5797c744fb
Duy Nguyen <duy.nguyen@christinas.vn>
2019-10-23 17:49:12 +0700

    Add CounterButton component into Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-
+import CounterButton from './CounterButton';
 class Header extends Component {
   shouldComponentUpdate(nextProps, nextState) {
     return false;
   }
 
   render() {
-    return <h1 className='f1'>RoboFriends</h1>;
+    return (
+      <React.Fragment>
+        <h1 className='f1'>RoboFriends</h1>
+        <CounterButton />
+      </React.Fragment>
+    );
   }
 }
 
